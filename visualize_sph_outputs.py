bd363281f285898a37b5fc75ba840e02490d2aa0
ScottHull <scottdhull21@gmail.com>
2020-05-07 18:04:29 -0400

    Added support for multiprocessed outputs.

--- visualize_sph_outputs.py
+++ visualize_sph_outputs.py
@@ -10,7 +10,7 @@ from mpl_toolkits.mplot3d import Axes3D
 class BuildMovie:
 
     def __init__(self, output_path, to_path, num_files, fps=10, start_from=0, new_sim=True, colorize_particles=True,
-                 dimension='3'):
+                 dimension='3', num_processes=1, focus_process=None):
         self.output_path = output_path
         self.to_path = to_path
         self.num_files = num_files
@@ -19,8 +19,11 @@ class BuildMovie:
         self.fps = fps
         self.colorize_particles = colorize_particles
         self.animation_filename = 'sph_output.mp4'
-        self.file_format = "results.{}_00001_00000.dat"
         self.dimension = dimension
+        self.num_processes = num_processes
+        self.curr_process = 0
+        self.focus_process = focus_process
+        self.file_format = "results.{}_0000{}_0000{}.dat"
         self.color_map = {
             0: 'red',
             1: 'blue',
@@ -33,7 +36,8 @@ class BuildMovie:
             os.mkdir(self.to_path)
 
     def __get_filename(self):
-        return self.output_path + "/" + self.file_format.format(str(self.curr_file).zfill(5))
+        return self.output_path + "/" + self.file_format.format(str(self.curr_file).zfill(5), self.num_processes,
+                                                                self.curr_process)
 
     def __read_sph_file(self):
         df = pd.read_csv(self.__get_filename(), sep='\t', skiprows=2, header=None)
@@ -47,28 +51,55 @@ class BuildMovie:
         return particle_id, x, y, z, colors
 
     def __make_scene(self, savefig=True, file_num=None, alpha=1.0):
-        if savefig:
-            particle_id, x, y, z, colors = self.__read_sph_file()
-        else:
-            savestate = copy(self.curr_file)
-            self.curr_file = file_num
-            particle_id, x, y, z, colors = self.__read_sph_file()
-            self.curr_file = savestate
         fig = plt.figure()
         fig.set_size_inches(18.5, 10.5)
         if self.dimension == '3':
             ax = Axes3D(fig)
-            if self.colorize_particles:
-                ax.scatter(x, y, z, c=colors, alpha=alpha)
+            if self.focus_process is None:
+                for proc in range(0, self.num_processes, 1):
+                    self.curr_process = proc
+                    if savefig:
+                        particle_id, x, y, z, colors = self.__read_sph_file()
+                    else:
+                        savestate = copy(self.curr_file)
+                        self.curr_file = file_num
+                        particle_id, x, y, z, colors = self.__read_sph_file()
+                        self.curr_file = savestate
+                    print(self.__get_filename())
+                    if self.colorize_particles:
+                        ax.scatter(x, y, z, c=colors, alpha=alpha)
+                    else:
+                        ax.scatter(x, y, z, c='black', alpha=alpha)
+                    self.curr_process = proc
             else:
-                ax.scatter(x, y, z, c='black', alpha=alpha)
+                self.curr_process = self.focus_process
+                if savefig:
+                    particle_id, x, y, z, colors = self.__read_sph_file()
+                else:
+                    savestate = copy(self.curr_file)
+                    self.curr_file = file_num
+                    particle_id, x, y, z, colors = self.__read_sph_file()
+                    self.curr_file = savestate
+                print(self.__get_filename())
+                if self.colorize_particles:
+                    ax.scatter(x, y, z, c=colors, alpha=alpha)
+                else:
+                    ax.scatter(x, y, z, c='black', alpha=alpha)
             ax.set_xlabel('x')
             ax.set_ylabel('y')
             ax.set_zlabel('z')
             # ax.set_xbound(-5e7, 5e7)
             # ax.set_ybound(-5e7, 5e7)
             # ax.set_zbound(-5e7, 5e7)
+
         else:
+            if savefig:
+                particle_id, x, y, z, colors = self.__read_sph_file()
+            else:
+                savestate = copy(self.curr_file)
+                self.curr_file = file_num
+                particle_id, x, y, z, colors = self.__read_sph_file()
+                self.curr_file = savestate
             ax = fig.add_subplot(111)
             if self.colorize_particles:
                 ax.scatter(x, y, c=colors, alpha=alpha)
@@ -87,14 +118,17 @@ class BuildMovie:
         else:
             plt.show()
 
+        self.curr_process = 0
+
     def __animate(self):
         frames = [self.to_path + "/output_{}.png".format(i) for i in range(self.start_file, self.num_files + 1)]
         animation = mpy.ImageSequenceClip(frames, fps=self.fps, load_images=True)
         animation.write_videofile('sph_output.mp4', fps=self.fps)
 
-    def build_animation(self):
+    def build_animation(self, save=False):
         for i in range(self.start_file, self.num_files + 1):
-            self.__make_scene(savefig=True, file_num=None)
+            self.__make_scene(savefig=save, file_num=i)
+            self.curr_process = 0
         self.__animate()
         print("Your animation is now available at {}/{}.".format(os.getcwd(), self.animation_filename))
         return None
@@ -110,13 +144,15 @@ path = "/Users/scotthull/Desktop/test2"
 mov = BuildMovie(
     output_path=path,
     to_path=os.getcwd() + "/sph_visualization",
-    num_files=1000,
+    num_files=280,
     fps=30,
     colorize_particles=True,
     dimension='3',
-    start_from=0
+    start_from=0,
+    num_processes=4,
+    focus_process=1
 )
 
-mov.build_animation()
+mov.build_animation(save=True)
 
 # mov.return_single_scene(file_num=0)
