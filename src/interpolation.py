c60376287f542311ca2dfccf85c2e35671b11405
ScottHull <scottdhull21@gmail.com>
2020-07-20 14:19:47 -0400

    Disk structure interpolation code.

--- src/interpolation.py
+++ src/interpolation.py
@@ -14,19 +14,10 @@ def __get_neighbors(val, array):
 def interpolate1d(val, val_array, interp_array):
     sorted_interp_array = [x for _, x in sorted(zip(val_array, interp_array))]
     sorted_val_array = list(reversed(val_array))
-    print()
 
     neighbor_indices = __get_neighbors(val=val, array=sorted_val_array)
     val_neighbors = (sorted_val_array[neighbor_indices[0]], sorted_val_array[neighbor_indices[1]])
     interp_neighbors = (sorted_interp_array[neighbor_indices[0]], sorted_interp_array[neighbor_indices[1]])
-    print(val, val_neighbors)
-    print(interp_neighbors)
+
     return (((val_neighbors[1] - val) / (val_neighbors[1] - val_neighbors[0])) * interp_neighbors[0]) + \
            (((val_neighbors[0] - val) / (val_neighbors[1] - val_neighbors[0])) * interp_neighbors[1])
-
-phase_df = pd.read_fwf("./phase_data/duniteS_vapour_curve.txt", skiprows=1,
-                                        names=["temperature", "density_sol_liq", "density_vap", "pressure",
-                                               "entropy_sol_liq", "entropy_vap"])
-
-i = interpolate1d(val=7.9993E+03, val_array=phase_df["temperature"], interp_array=phase_df["entropy_vap"])
-print(i)
