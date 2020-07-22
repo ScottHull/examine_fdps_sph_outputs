e34d302919e59488f6e9f32fb2141b48ecbdfe80
ScottHull <scottdhull21@gmail.com>
2020-07-22 14:03:00 -0400

    Better planet centering in particles_to_planets.py.

--- calculate_disk_structure.py
+++ calculate_disk_structure.py
@@ -13,3 +13,13 @@ disk_particles = [p for p in particle_map if p.label == "DISK"]
 s = Structure(disk_particles=disk_particles)
 vmf = s.calc_vapor_mass_fraction()
 print(vmf)
+surface_densities, sorted_distances = s.calc_disk_surface_density()
+
+ax = plt.figure().add_subplot(111)
+ax.plot([i / 1000.0 for i in sorted_distances], surface_densities, color="black", linewidth=2.0)
+ax.set_xlabel("Distance from Earth Center (km)")
+ax.set_ylabel("Disk Surface Density (kg/m3)")
+ax.set_title("Disk + Escaping Particles Surface Density")
+ax.grid()
+
+plt.show()
--- src/structure.py
+++ src/structure.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from math import pi
+import numpy as np
 from src.interpolation import interpolate1d
 
 
@@ -22,7 +24,6 @@ class Structure:
                                         interp_array=self.phase_df['entropy_sol_liq'])
             entropy_vap = interpolate1d(val=temperature_i, val_array=self.phase_df['temperature'],
                                         interp_array=self.phase_df['entropy_vap'])
-            print(temperature_i, entropy_i, entropy_liq, entropy_vap)
             if entropy_i < entropy_liq:
                 vapor_mass_fraction += 0.0
             elif entropy_liq <= entropy_i <= entropy_vap:
@@ -32,3 +33,18 @@ class Structure:
         vapor_mass_fraction = vapor_mass_fraction / num_particles
 
         return vapor_mass_fraction
+
+    def calc_disk_surface_density(self):
+        particles = sorted_interp_array = [x for _, x in sorted(zip(self.disk_particles,
+                                                                    [i.distance for i in self.disk_particles]))]
+        surface_densities = []
+        for index, p in enumerate(particles):
+            if index > 0:
+                dM = p.mass - particles[index - 1].mass
+                dr = p.distance = particles[index - 1].distance
+                sd = dM / (2.0 * pi * p.distance * dr)
+                surface_densities.append(sd)
+        return surface_densities, [p.distance for p in particles]
+
+
+
