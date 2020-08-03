0e1d29e91e23c763272fc57f22c2c8421628082c
ScottHull <scottdhull21@gmail.com>
2020-08-03 16:24:13 -0400

    Two-body inspection code in identify.py.

--- src/plots.py
+++ src/plots.py
@@ -6,7 +6,7 @@ def plot_heatmap(x, y, z, a, b, center):
     ax = plt.figure().add_subplot(111)
     sc = ax.scatter(x, y, c=z, marker="+")
     e = Ellipse(xy=(0, 0), width=a * 2.0, height=b * 2.0, alpha=0.2, color="blue")
-    ax.scatter(center[0], center[1], marker="*", color="red", size=60)
+    ax.scatter(center[0], center[1], marker="*", color="red", s=60)
     ax.add_artist(e)
     cbar = plt.colorbar(sc, ax=ax)
 
