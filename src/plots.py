913c3c3da60c69997b60ade5f7a64562b26a021e
ScottHull <scottdhull21@gmail.com>
2020-08-03 15:17:41 -0400

    Two-body inspection code in identify.py.

--- src/plots.py
+++ src/plots.py
@@ -4,7 +4,7 @@ import numpy as np
 def plot_heatmap(x, y, z):
     ax = plt.figure().add_subplot(111)
     y, x = np.meshgrid(x, y)
-    z = z[:-1, :-1]
+    z = z[1:-1]
     c = ax.pcolormesh(x, y, z, cmap='RdBu', vmin=min(z), vmax=max(z))
     # set the limits of the plot to the limits of the data
     cbar = plt.colorbar(c, ax=ax)
