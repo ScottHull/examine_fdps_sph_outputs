864cc807a86e1b7b08ea508f53c3af64c57e2240
ScottHull <scottdhull21@gmail.com>
2020-08-04 14:02:40 -0400

    Added profile_target.py.

--- profile_target.py
+++ profile_target.py
@@ -11,11 +11,19 @@ pm = ParticleMap(output_path=sph_file, number_expected_bodies=1)
 particle_map = pm.solve()
 
 ax1 = plots.scatter_particles(
-    x=[i.distance / 1000.0 for i in particle_map],
-    y=[i.entropy for i in particle_map],
-    tags=[i.particle_id for i in particle_map],
+    x=[i.distance / 1000.0 for i in particle_map if i.label == "PLANET"],
+    y=[i.entropy for i in particle_map if i.label == "PLANET"],
+    tags=[i.particle_id for i in particle_map if i.label == "PLANET"],
     x_label="Distance (km)",
     y_label="Entropy"
+)
+
+ax2 = plots.scatter_particles(
+    x=[i.position_vector[0] / 1000.0 for i in particle_map],
+    y=[i.position_vector[1] / 1000.0 for i in particle_map],
+    tags=[i.particle_id for i in particle_map],
+    x_label="x (km)",
+    y_label="y (km)"
 )
 
 plt.show()
