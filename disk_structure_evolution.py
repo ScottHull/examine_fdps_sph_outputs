eaee4da2acfe1fc9b8dc67865d1266b08f97fe4d
ScottHull <scottdhull21@gmail.com>
2020-10-09 15:33:40 -0400

    particle report

--- disk_structure_evolution.py
+++ disk_structure_evolution.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import numpy as np
 from src.identify import ParticleMap
@@ -14,6 +15,8 @@ path_to_outputs = "/scratch/shull4/GI2/"
 for time in np.arange(start_time, end_time + interval, interval):
     print("Generating report for time: {}".format(time))
     combined_file = CombineFile(num_processes=number_processes, time=time, output_path=path_to_outputs).combine()
-    pm = ParticleMap(output_path=combined_file, center_on_target_iron=True, plot=True)
+    f = os.getcwd() + "/merged_{}.dat".format(time)
+    pm = ParticleMap(output_path=f, center_on_target_iron=True, plot=True)
     particle_map = pm.solve()
     make_report(particles=particle_map, time=time, to_directory="/particle_outputs")
+    os.remove(f)
