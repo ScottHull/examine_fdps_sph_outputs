ed7521dc06f5f6fe8f4b8567b60ac8798e212054
ScottHull <scottdhull21@gmail.com>
2020-10-16 14:20:18 -0400

    Added 3D plotting.

--- compare_dunite_eos.py
+++ compare_dunite_eos.py
@@ -28,4 +28,24 @@ for i in paths:
 ax.grid()
 ax.legend()
 
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+ax2.set_xlabel("Internal Energy")
+ax2.set_ylabel("Soundspeed")
+ax2.set_title("Compare EOS")
+for i in paths:
+    path, name = i
+    eos_df = pd.read_fwf(path, header=None, skiprows=2)
+    internal_energy, soundspeed = eos_df[2], eos_df[4]
+    ax2.scatter(
+        internal_energy,
+        soundspeed,
+        marker="+",
+        label=name
+    )
+
+ax2.grid()
+ax2.legend()
+
+
 plt.show()
