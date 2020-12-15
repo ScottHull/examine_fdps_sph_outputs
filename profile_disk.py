b6cdb0900d1dd60e695bce1606931525bf5340e9
ScottHull <scottdhull21@gmail.com>
2020-12-15 15:52:48 -0500

    Updated report.py.

--- profile_disk.py
+++ profile_disk.py
@@ -66,9 +66,9 @@ fig.clear()
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.scatter(escaping_end_positions, escaping_entropies[-1], marker="+",
-        color="red", label="Escaping Entropy")
+        color="red", label="Escaping Entropy", alpha=0.3)
 ax.scatter(disk_end_positions, disk_entropies[-1], marker="+",
-        color="blue", label="Disk Entropy")
+        color="blue", label="Disk Entropy", alpha=0.3)
 ax.set_xlabel("Time (iteration)")
 ax.set_ylabel("Entropy")
 ax.set_title("Disk and Escaping Entropy at Iteration {}".format(end_time))
