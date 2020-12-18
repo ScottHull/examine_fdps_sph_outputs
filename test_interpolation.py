c0d55ea0ac21d6675e87d884b294964c44087243
ScottHull <scottdhull21@gmail.com>
2020-12-18 16:39:35 -0500

    Added test_interpolation.py.

--- test_interpolation.py
+++ test_interpolation.py
@@ -9,13 +9,23 @@ path_to_outputs = "/scratch/shull4/impactor/"
 number_processes = 100
 time = 1000
 
-eos_df = pd.read_fwf("src/eos/duniteS2.rho_u.txt", header=None, skiprows=2)
-density = list(eos_df[0])  # load in the full-length density array from eos_df
-energy = list(eos_df[1])  # load in the full-length energy array from eos_df
-entropy = list(eos_df[5])  # load in the full-length entropy array from eos_df
-tempertaure = list(eos_df[2])
-pressure = list(eos_df[3])
-soundspeed = list(eos_df[4])
+eos_df_silicate = pd.read_fwf("src/eos/duniteS2.rho_u.txt", header=None, skiprows=2)
+density_silicate = list(eos_df_silicate[0])  # load in the full-length density array from eos_df_silicate
+energy_silicate = list(eos_df_silicate[1])  # load in the full-length energy array from eos_df_silicate
+entropy_silicate = list(eos_df_silicate[5])  # load in the full-length entropy array from eos_df_silicate
+tempertaure_silicate = list(eos_df_silicate[2])
+pressure_silicate = list(eos_df_silicate[3])
+soundspeed_silicate = list(eos_df_silicate[4])
+
+eos_df_iron = pd.read_fwf("src/eos/iron___.rho_u.txt", header=None, skiprows=2)
+density_iron = list(eos_df_iron[0])  # load in the full-length density array from eos_df_iron
+energy_iron = list(eos_df_iron[1])  # load in the full-length energy array from eos_df_iron
+entropy_iron = list(eos_df_iron[5])  # load in the full-length entropy array from eos_df_iron
+tempertaure_iron = list(eos_df_iron[2])
+pressure_iron = list(eos_df_iron[3])
+soundspeed_iron = list(eos_df_iron[4])
+
+
 
 combined_file = CombineFile(num_processes=number_processes, time=time, output_path=path_to_outputs).combine()
 sph_file = os.getcwd() + "/merged_{}.dat".format(time)
@@ -26,16 +36,23 @@ given_internal_energy = []
 interpolated_internal_energy = []
 given_entropy = []
 for row in sph_df.index:
+    tag_sph = sph_df[1][row]
     density_sph = sph_df[9][row]
     internal_energy_sph = sph_df[10][row]
     entropy_sph = sph_df[13][row]
     given_internal_energy.append(internal_energy_sph)
     given_density.append(density_sph)
     given_entropy.append(entropy_sph)
-    interpolated_val = GenericTrilinearInterpolation(var1_array=density, var2_array=entropy,
-                                  var3_array=energy,
-                                  var1=density_sph, var2=entropy_sph,
-                                  grid_length=120).interpolate()
+    if tag_sph % 2 == 0:
+        interpolated_val = GenericTrilinearInterpolation(var1_array=density_silicate, var2_array=entropy_silicate,
+                                      var3_array=energy_silicate,
+                                      var1=density_sph, var2=entropy_sph,
+                                      grid_length=120).interpolate()
+    else:
+        interpolated_val = GenericTrilinearInterpolation(var1_array=density_iron, var2_array=entropy_iron,
+                                                         var3_array=energy_iron,
+                                                         var1=density_sph, var2=entropy_sph,
+                                                         grid_length=120).interpolate()
     interpolated_internal_energy.append(interpolated_val)
 
 
