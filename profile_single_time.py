000258c0f638a172a86af85da2beb35709412073
ScottHull <scottdhull21@gmail.com>
2021-04-23 16:51:35 -0400

    added profile_single_time.py.

--- profile_single_time.py
+++ profile_single_time.py
@@ -11,15 +11,14 @@ import shutil
 
 time = 2000
 number_processes = 100
-# path_to_outputs = "/scratch/shull4/gi"
-path_to_outputs = "/Users/scotthull/Desktop"
+path_to_outputs = "/scratch/shull4/gi"
 
-# combined_file = CombineFile(num_processes=number_processes, time=time, output_path=path_to_outputs).combine()
-# f = os.getcwd() + "/merged_{}.dat".format(time)
-# pm = ParticleMap(output_path=f, center_on_target_iron=True, plot=False, relative_velocity=True, center_plot=True)
-# particle_map = pm.solve()
-pm = ParticleMapFromFiles(path=path_to_outputs)
-particle_map = pm.read(time=time)
+combined_file = CombineFile(num_processes=number_processes, time=time, output_path=path_to_outputs).combine()
+f = os.getcwd() + "/merged_{}.dat".format(time)
+pm = ParticleMap(output_path=f, center_on_target_iron=True, plot=False, relative_velocity=True, center_plot=True)
+particle_map = pm.solve()
+# pm = ParticleMapFromFiles(path=path_to_outputs)
+# particle_map = pm.read(time=time)
 
 fig = plots.plot_eccentricities(
     particles=particle_map,
@@ -29,4 +28,14 @@ fig = plots.plot_eccentricities(
     b=1e6,
 )
 fig.savefig("eccentricity_{}.png".format(time), format='png')
-plt.close()
+
+fig = plots.colorcode_orbits(
+        particles=particle_map,
+        # a=pm.a,
+        # b=pm.b,
+        a=1e6,
+        b=1e6,
+        center_plot=True,
+        z=True
+    )
+fig.savefig("orbits_{}.png".format(time), format='png')
