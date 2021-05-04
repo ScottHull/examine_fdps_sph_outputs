b78d3bf825578c50cdbc7d8ce1b31dea978960f5
ScottHull <scottdhull21@gmail.com>
2021-05-04 13:01:21 -0400

    added find_center_of_mode1_bodies.py.

--- find_center_of_mode1_bodies.py
+++ find_center_of_mode1_bodies.py
@@ -13,6 +13,25 @@ f = os.getcwd() + "/merged_{}.dat".format(time)
 pm = ParticleMap(output_path=f, center_on_target_iron=True, plot=False, relative_velocity=True,
                  center_plot=True).collect_all_particles(find_orbital_elements=False)
 
+
+
+
+def line_intersection(line1, line2):
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+       raise Exception('lines do not intersect')
+
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    return x, y
+
 impactor = [i for i in pm if i.particle_id > 1]
 target = [i for i in pm if i.particle_id <= 1]
 
@@ -38,6 +57,18 @@ mid_x_tar = (max_x_tar + min_x_tar) / 2
 mid_y_tar = (max_y_tar + min_y_tar) / 2
 mid_z_tar = (max_z_tar + min_z_tar) / 2
 
+imp_min_x_coord = (min_x_imp, mid_y_imp)
+imp_max_x_coord = (max_x_imp, mid_y_imp)
+imp_min_y_coord = (mid_x_imp, min_y_imp)
+imp_max_y_coord = (mid_x_imp, max_y_imp)
+tar_min_x_coord = (min_x_tar, mid_y_tar)
+tar_max_x_coord = (max_x_tar, mid_y_tar)
+tar_min_y_coord = (mid_x_tar, min_y_tar)
+tar_max_y_coord = (mid_x_tar, max_y_tar)
+
+imp_intersection = line_intersection((imp_min_x_coord, imp_max_x_coord), (imp_min_y_coord, imp_max_y_coord))
+tar_intersection = line_intersection((tar_min_x_coord, tar_max_x_coord), (tar_min_y_coord, tar_max_y_coord))
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.scatter(
@@ -45,19 +76,21 @@ ax.scatter(
     [i.position_vector[1] for i in impactor],
     marker="+",
     color='red',
-    label="IMPACTOR"
+    label="IMPACTOR",
+    alpha=0.8
 )
 ax.scatter(
     [i.position_vector[0] for i in target],
     [i.position_vector[1] for i in target],
     marker="+",
-    color='red',
-    label="TARGET"
+    color='blue',
+    label="TARGET",
+    alpha=0.8
 )
 
 ax.plot(
     [min_x_imp, max_x_imp],
-    [mid_x_imp, mid_x_imp],
+    [mid_y_imp, mid_y_imp],
     linewidth=2.0,
     color='green'
 )
@@ -80,9 +113,21 @@ ax.plot(
     linewidth=2.0,
     color='pink'
 )
+ax.plot(
+    [imp_intersection[0], tar_intersection[0]],
+    [imp_intersection[1], tar_intersection[1]],
+    linewidth=2.0,
+    linestyle="--",
+    color='black'
+)
 ax.set_xlabel("x")
 ax.set_xlabel("y")
 ax.set_title("Iteration: {}".format(time))
 ax.grid()
 ax.legend()
 plt.savefig("mode1_dimensions.png", format='png')
+
+print(
+    "TARGET CENTER COORDS: {}\n"
+    "IMPACTOR CENTER COORDS: {}".format(tar_intersection, imp_intersection)
+)
